4c699f8182694c7e4631e9d50aa78950c3add9ad
Nick Pruitt <pruitt.nick@gmail.com>
2025-11-19 10:35:03 -0600

    Generic semantic type

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,9 @@
+/**
+ * Shared type definitions used across components
+ */
+
+/**
+ * HTML heading levels for semantic markup
+ */
+export type SemanticLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
