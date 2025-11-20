f38f5eae06c2d403317d0a6e02ea74bc3add8737
Nick Pruitt <pruitt.nick@gmail.com>
2025-11-20 07:43:35 -0600

    Server Islands Demo

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [solidJs()],
   image: {
     domains: ['cdn.sanity.io'],
