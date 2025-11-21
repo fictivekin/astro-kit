3cb060cefdd795a8eaf60033becd4c1a7ab00c44
Nick Pruitt <pruitt.nick@gmail.com>
2025-11-21 14:33:57 -0600

    Add sitemap

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,22 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import solidJs from '@astrojs/solid-js';
-import node from '@astrojs/node';
+import solidJs from "@astrojs/solid-js";
+import node from "@astrojs/node";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [solidJs()],
+  site: "http://localhost:4321", // Replace with your actual domain
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  integrations: [
+    solidJs(),
+    sitemap({
+      filter: (page) => !page.includes("/preview"),
+    }),
+  ],
   image: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
   },
   vite: {
     resolve: {
       alias: {
-        '@/components': '/src/components',
-        '@': '/src',
+        "@/components": "/src/components",
+        "@": "/src",
       },
     },
   },
